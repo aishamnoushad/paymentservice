8088477594840d5f35250b09864a401df833f509
abcgroupuae <media@abcgroup.ae>
2021-06-24 15:31:09 +0400

    package name issue sorted

--- payment-service/src/main/java/com/aisha/ps/api/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/aisha/ps/api/PaymentServiceApplication.java
@@ -1,4 +1,4 @@
-package com.aisha.paymentservice;
+package com.aisha.ps.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
